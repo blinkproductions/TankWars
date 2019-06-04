17f357f7d0bbca8f977e845cbcfe8e33309825f3
oO-Blink-Oo <alvarezian23@gmail.com>
2019-06-04 09:12:03 -0700

    added more functionality for TankPlayerController

--- TankWars/Source/TankWars/TankPlayerController.cpp
+++ TankWars/Source/TankWars/TankPlayerController.cpp
@@ -35,8 +35,19 @@ ATank * ATankPlayerController::GetControlledTank() const
 void ATankPlayerController::AimTowardsCrosshair() 
 {
 	if (!GetControlledTank()) { return; }
+	
+	FVector HitLocation; // Out parameter
+	if (GetSightRayHitLocation(HitLocation)) // Has a "side-effect", is going to line trace
+	{
+		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString())
 
-	// Get world location if linetrace through crosshair
-	// If it hits the landscape
-		// Tell controlled tank to aim at this point
+			// TODO Tell controlled tank to aim at this point
+	}
+}
+
+// Get world location of linetrace through crosshair, true if hits landscape
+bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) const 
+{
+	OutHitLocation = FVector(1.0);
+	return true;
 }
