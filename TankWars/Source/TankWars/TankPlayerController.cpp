706df9a6cc81ff821b8fd15f3209dcfdd887f83f
oO-Blink-Oo <alvarezian23@gmail.com>
2019-06-04 09:33:19 -0700

    updated TankPlayerController

--- TankWars/Source/TankWars/TankPlayerController.cpp
+++ TankWars/Source/TankWars/TankPlayerController.cpp
@@ -39,15 +39,20 @@ void ATankPlayerController::AimTowardsCrosshair()
 	FVector HitLocation; // Out parameter
 	if (GetSightRayHitLocation(HitLocation)) // Has a "side-effect", is going to line trace
 	{
-		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString())
-
-			// TODO Tell controlled tank to aim at this point
+		//UE_LOG(LogTemp, Warning, TEXT("Look direction: %s"), *HitLocation.ToString())
+		// TODO Tell controlled tank to aim at this point
 	}
 }
 
 // Get world location of linetrace through crosshair, true if hits landscape
 bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) const 
 {
-	OutHitLocation = FVector(1.0);
+	// Find the crosshair position
+	int32 ViewpoerSizeX, ViewportSizeY;
+	GetViewportSize(ViewpoerSizeX, ViewportSizeY);
+	auto ScreenLocation = FVector2D(ViewpoerSizeX * CrossHairXLocation, ViewportSizeY * CrossHairYLocation);
+	
+	// "De-project" the screen position of the crosshair to a world direction
+	// Line-trace along that look direction, and see what we hit (up to max range)
 	return true;
 }
--- TankWars/Source/TankWars/TankPlayerController.h
+++ TankWars/Source/TankWars/TankPlayerController.h
@@ -17,13 +17,20 @@ class TANKWARS_API ATankPlayerController : public APlayerController
 	
 private:
 	ATank * GetControlledTank() const;
-	virtual void BeginPlay() override;
-	virtual void Tick(float DeltaTime) override;
-	
-	
+
 	// Start the tank moving the barrel so that a shot would hit where
 	// the crosshair intersects the world
 	void AimTowardsCrosshair();
 
 	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
+
+public:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+	float CrossHairXLocation = 0.5f;
+	
+	UPROPERTY(EditAnywhere)
+	float CrossHairYLocation = 0.33333f;
 };
